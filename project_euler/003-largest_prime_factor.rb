4b312b50edf44ed2ceca7bbe7d4ed33cd5039f05
Harish Ramachandran <harishsr.coder@gmail.com>
2016-09-07 16:47:13 -0400

    pu 003 -- rb, complete

--- project_euler/003-largest_prime_factor.rb
+++ project_euler/003-largest_prime_factor.rb
@@ -1,4 +1,13 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
+require 'prime'
 
+class PrimeFactor < Prime
+  def self.largest( num )
+    factors = prime_division(num)
+    factors.flatten.sort[-1]
+  end
+end
+
+puts PrimeFactor.largest(600851475143)
