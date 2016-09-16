f88b8c0f32b764560515191d9264f5cdc4e8580a
Harish Ramachandran <harishsr.coder@gmail.com>
2016-09-16 15:04:03 -0400

    pu 003 -- js, wrote primeFactor function...wip

--- project_euler/003-largest_prime_factor.js
+++ project_euler/003-largest_prime_factor.js
@@ -1,3 +1,26 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
 
+var math = require('mathjs')
+
+//
+// go through all the factors of a number and get an array of primes
+function primeFactor(num){
+  var allFactors = factors(num);
+  var primeFactors = [];
+
+  for(i = 0; i < num; i++){
+    if (math.isPrime(allFactors[i])) {
+      primeFactors.push(allFactors[i]);
+    }
+  }
+
+  return primeFactors;
+}
+
+//
+// calculate all the factors of a number
+function factors(num){
+}
+
+PrimeFactor(13195);
